41ed6d8636bd294440a0e6daa2de691808f1ca19
KaoruOnuki <kaoru.onuki@gmail.com>
2018-06-18 20:00:02 +0900

    Create basic flow of the idea mixer

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,4 +1,27 @@
 class IdeasController < ApplicationController
   def index
+    @cards = Card.all
+    @randoms = Card.order("RANDOM()").limit(2)
+
+    @ideas = Idea.all
+    @idea = Idea.new
+  end
+
+  def create
+    @idea = Idea.new(idea_params)
+    respond_to do |format|
+      if @idea.save
+        format.html { redirect_to ideas_path }
+        format.json { render :show, status: :created, location: @idea }
+      else
+        format.html { render :new }
+        format.json { render json: @idea.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+private
+  def idea_params
+    params.require(:idea).permit(:card_one, :card_two, :idea_content)
   end
 end
