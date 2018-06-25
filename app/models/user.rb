96d2e8c25cb4238df67667443762f824169eb1dd
KaoruOnuki <kaoru.onuki@gmail.com>
2018-06-25 18:57:36 +0900

    Set up User function including secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   before_save { name.downcase! }
+  validates :name, presence: true, length: { in: 1..30 }
+
+  has_secure_password
+  validates :password, length: { minimum: 6 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root "cards#index"
   resources :cards
   resources :ideas
+
+  resources :users, only: [:new, :create, :show]
 end
