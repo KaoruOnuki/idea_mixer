a6b62f10cf159bf3be96bbdb8e3252fba0d84eae
KaoruOnuki <kaoru.onuki@gmail.com>
2018-06-25 18:23:12 +0900

    List out the idea_content that covers card_one and card_two selected upon refreshing the page

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,3 @@
 class Card < ApplicationRecord
+  validates :content, length: { in: 1..15 }, uniqueness: { case_sensitive: false }
 end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,2 +1,9 @@
 class Idea < ApplicationRecord
+  before_validation :delete_space
+  validates :idea_content, length: { in: 1..30 }, uniqueness: { case_sensitive: false }
+
+private
+  def delete_space
+    idea_content.gsub!(" ", "")
+  end
 end
