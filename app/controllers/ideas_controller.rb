091df6a6bdf812d5eb85bafe32171b6ea7858dba
KaoruOnuki <kaoru.onuki@gmail.com>
2018-06-27 16:36:10 +0900

    Set up a function on idea_content editting

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,27 +1,28 @@
 class IdeasController < ApplicationController
-  before_action :your_page, only: [:index, :new, :edit, :show, :destroy]
-  # before_action :set_idea, only: [:show, :edit, :update, :destroy]
+  before_action :your_page, only: [:index, :new, :show, :destroy]
+  before_action :set_idea, only: [:show, :edit, :update, :destroy]
+
   def index
     @ideas = Idea.all
     @idea = Idea.new
 
     @cards = Card.all
-    @randoms = Card.order("RANDOM()").limit(2)
 
+    @randoms = Card.order("RANDOM()").limit(2)
     @selected_on_card_one = @ideas.where(["card_one = ? or card_two = ?", @randoms[0].content, @randoms[0].content])
     @selected_on_card_two = @ideas.where(["card_one = ? or card_two = ?", @randoms[1].content, @randoms[1].content])
   end
 
 
-  # def show
-  # end
-  #
-  # def new
-  #   @idea = Idea.new
-  # end
-  #
-  # def edit
-  # end
+  def show
+  end
+
+  def new
+    @idea = Idea.new
+  end
+
+  def edit
+  end
 
   def create
     @ideas = Idea.all
@@ -44,30 +45,30 @@ class IdeasController < ApplicationController
     end
   end
 
-  # def update
-  #   respond_to do |format|
-  #     if @idea.update(idea_params)
-  #       format.html { redirect_to @idea, notice: 'idea was successfully updated.' }
-  #       format.json { render :show, status: :ok, location: @idea }
-  #     else
-  #       format.html { render :edit }
-  #       format.json { render json: @idea.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-  #
-  # def destroy
-  #   @idea.destroy
-  #   respond_to do |format|
-  #     format.html { redirect_to ideas_url, notice: 'idea was successfully destroyed.' }
-  #     format.json { head :no_content }
-  #   end
-  # end
+  def update
+    respond_to do |format|
+      if @idea.update(idea_params)
+        format.html { redirect_to @idea, notice: 'idea was successfully updated.' }
+        format.json { render :show, status: :ok, location: @idea }
+      else
+        format.html { render :edit }
+        format.json { render json: @idea.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @idea.destroy
+    respond_to do |format|
+      format.html { redirect_to ideas_url, notice: 'idea was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
 
 private
-  # def set_idea
-  #   @idea = Idea.find(params[:id])
-  # end
+  def set_idea
+    @idea = Idea.find(params[:id])
+  end
 
   def idea_params
     params.require(:idea).permit(:card_one, :card_two, :idea_content)
